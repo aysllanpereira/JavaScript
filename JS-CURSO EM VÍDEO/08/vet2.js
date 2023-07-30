0396e561691548e4942aef03519818074c3b55c8
aysllanpereira <aysllansantos097@gmail.com>
2023-07-30 19:17:50 -0300

    Praticando

--- JS-CURSO EM VÍDEO/08/vet2.js
+++ JS-CURSO EM VÍDEO/08/vet2.js
@@ -1,5 +1,18 @@
-let valores = [8, 5, 9, 10, 6, 2, 4];
-valores.sort();
+function verificar() {
+
+    let valores = [8, 5, 9, 10, 6, 2, 4];
+    let num = parseInt(document.getElementById("inum").value, 10)
+    let res = document.getElementById("res");
+
+    if (num != valores.includes(num)) {
+        res.innerHTML = `O número digitado está entre os selecionados no programa!`
+    } else {
+        res.innerHTML = `Ops... Escolheu o número errado, mais sorte na próxima!`
+    }
+}
+
+
+
 
 //console.log(valores[0]);
 
@@ -7,6 +20,7 @@ valores.sort();
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`);
 }*/
 
-for (let pos in valores) {
+/*for (let pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`);
-}
+}*/
+
