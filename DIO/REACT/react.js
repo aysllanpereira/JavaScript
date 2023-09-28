4ca97bf9034d809ee0b0da51847c5f4a55277b1c
aysllanpereira <aysllansantos097@gmail.com>
2023-09-27 23:09:11 -0300

    Revendo condicionais em JS

---
+++ DIO/REACT/react.js
@@ -0,0 +1,17 @@
+const nota = 6;
+
+/* switch(nota) {
+    case nota < 5:
+        console.log(`Abaixo da média`);
+    break;    
+
+    case nota > 5:
+        console.log(`Na média`);
+    break;
+    
+    default:
+        console.log(`Acima da média`);
+} */
+
+/* condição ternária */
+nota > 5 ? console.log(`Aprovrado`) : console.log(`Reprovado`);
