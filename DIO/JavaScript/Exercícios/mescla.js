70e8e8d90c035eee1379936900d900d46abc6da4
aysllanpereira <aysllansantos097@gmail.com>
2023-10-04 21:34:10 -0300

    Montanto um exercício de mesclagem

--- DIO/JavaScript/Exercícios/mescla.js
+++ DIO/JavaScript/Exercícios/mescla.js
@@ -7,23 +7,24 @@ function mergeStrings(word1, word2) {
       merged += word1[i] + word2[j];
       i++;
       j++;
+
     }
-  
+
     while (i < word1.length) {
-      merged += word1[i];
+      merged = word1[i];
       i++;
     }
-  
+
     while (j < word2.length) {
-      merged += word2[j];
+      merged = word2[j];
       j++;
     }
-  
+
     return merged;
-  }
   
-  // Test cases
-  console.log(mergeStrings("abc", "pqr")); // Output: "apbqcr"
-  console.log(mergeStrings("ab", "pqrs")); // Output: "apbqrs"
-  console.log(mergeStrings("abcd", "pq")); // Output: "apbqcd"
+}
+
+console.log(mergeStrings(`abc`, `pqr`));
+console.log(mergeStrings(`uxr`, `kop`));
+console.log(mergeStrings(`cmh`, `lço`));
   
