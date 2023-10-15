0c9bde438ea946277f144b5bcb9e7665b806032a
aysllanpereira <aysllansantos097@gmail.com>
2023-10-14 23:53:02 -0300

    Organizando e começando a deixar o projeto responsivo

--- DIO/JavaScript/Exercícios/multa.js
+++ DIO/JavaScript/Exercícios/multa.js
@@ -1,27 +1,31 @@
-
-
 function calcular() {
-    // entradas convertidas 
-    let contrato = parseInt(document.getElementById("icontrato").value)
-    let dataQuitacao = moment(document.getElementById("idata").value);
-    let prazo = parseFloat(document.getElementById("iprazo").value);
-    let valorPago = parseFloat(document.getElementById("ivalor").value);
-    let dataDoDia = moment(document.getElementById("idatadodia").value);
-   
+
+    // validação das entradas 
+    const contrato = parseInt(document.getElementById("icontrato").value);
+    const prazo = parseFloat(document.getElementById("iprazo").value);
+    const valorPago = parseFloat(document.getElementById("ivalor").value);
+    
+   // conversão de datas 
+    const dataDoDia = moment(document.getElementById("idatadodia").value);
+    const dataQuitacao = moment(document.getElementById("idata").value);
 
     // calculos 
-    let multiplicar = (valorPago * 0.01);
-    let diasEmAtraso = dataDoDia - dataQuitacao;
-    let diferencaEmDias = Math.floor(Math.abs(diasEmAtraso) / (1000 * 60 * 60 * 24));
-    let final = (multiplicar / prazo) * diferencaEmDias;
+    const multiplicar = (valorPago * 0.01);
+    const diasEmAtraso = dataDoDia - dataQuitacao;
+    const diferencaEmDias = Math.floor(Math.abs(diasEmAtraso) / (1000 * 60 * 60 * 24));
+    const final = (multiplicar / prazo) * diferencaEmDias;
 
     // se não informar um valor correto, irá exibir um alerta
     if(isNaN(prazo)) {
         alert("Por favor, insira um valor numérico válido para o prazo");
         return;
     }
+
     // adicionando o prazo 
    dataQuitacao.add(prazo, 'days');
-   let resultado = document.getElementById("resultado");
+
+   // atualização do resultado da página
+   const resultado = document.getElementById("resultado");
    resultado.innerHTML = `O resultado é: ${dataQuitacao.format("DD/MM/YYYY")} e está a ${diferencaEmDias} dias em atraso e o valor é: ${final.toFixed()}`;
+
 }
