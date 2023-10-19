744e2318e82d12a3dccd725ff80bbeef7750e837
aysllanpereira <aysllansantos097@gmail.com>
2023-10-18 21:54:37 -0300

    Melhorando o código JS e adicionando mais estilo no CSS

--- DIO/JavaScript/POKEDEX/assets/JS/main.js
+++ DIO/JavaScript/POKEDEX/assets/JS/main.js
@@ -5,15 +5,15 @@ function converterPokemonTypesToLi (pokemonTypes) {
 function converterPokemonToLi (pokemon) {
     
     return `
-            <li class="pokemon">
-                <span class="number">#${pokemon.order}</span>
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        ${converterPokemonTypesToLi(pokemon.types).join('')}
+                        ${pokemon.types.map((type) => `<li class"type ${type}">${type}</li>`).join("")}
                     </ol>
-                    <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
                 </div>
             </li>
 
